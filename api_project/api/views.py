ec2d89c801547f2403e750a5ac0ddf0c9ea65b37
Ebo Mensah <ernestmensah02@gmail.com>
2024-11-20 00:15:55 +0000

    Task 1 DJANGO-REST framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import BookSerializer
+from rest_framework import generics 
+from .models import Book
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 # Create your views here.
