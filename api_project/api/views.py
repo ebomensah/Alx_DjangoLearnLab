f0c516fc2718a5b75f4be3e2e6b28781451e01fa
Ebo Mensah <ernestmensah02@gmail.com>
2024-11-21 23:11:25 +0000

    Task 2 of Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,22 @@
+from turtle import title
 from django.shortcuts import render
 from .serializers import BookSerializer
-from rest_framework import generics 
+from rest_framework import viewsets, generics
 from .models import Book
 
 class BookList (generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def get_queryset(self):
+        queryset = Book.objects.all()
+        title_filter = self.request.query_params.get('title', None)
+        if title_filter is not None:
+            queryset = queryset.filter (title__icontains=title_filter)
+        return queryset 
 # Create your views here.
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
