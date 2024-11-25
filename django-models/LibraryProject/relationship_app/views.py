392773b0064da001ff8f129d185182be808666f1
Ebomensah <ernestmensah02@gmail.com>
2024-11-25 01:43:29 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -41,7 +41,7 @@ def list_books(request):
     books = Book.objects.all()
     return render (request, 'relationship_app/list_books.html', {'books':books})
 
-class LibraryListView(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
 
