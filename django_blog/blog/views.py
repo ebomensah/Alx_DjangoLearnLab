a8ca016797d1366cafc02470e78a4d9c95d40f98
Ebo Mensah <ernestmensah02@gmail.com>
2025-01-19 00:15:45 +0000

    Adding Comment functionality to blog

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -15,7 +15,7 @@ from .models import Post, Comment
 from .forms import CommentForm
 
 @login_required
-def add_comment(request, post_id):
+def CommentCreateView(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         form = CommentForm(request.POST)
