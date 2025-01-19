e76c5bd25fea170d852a358c6669acafbe85202b
Ebo Mensah <ernestmensah02@gmail.com>
2025-01-19 20:55:14 +0000

    Adding Tag and search functions to blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -38,8 +38,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(attrs={'class': 'form-control', 'placeholder': 'Add tags...'}),
-        }
+            'tags': TagWidget()}
 
 class CommentForm(forms.ModelForm):
     class Meta:
